5ff94200c19097edbb738cc2fc378e7925b0e2c3
63070086 <69414436+63070086@users.noreply.github.com>
2020-11-14 00:30:42 +0700

    Update del_menu.php

--- del_menu.php
+++ del_menu.php
@@ -2,7 +2,7 @@
 $id=$_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'databaseitf.mysql.database.azure.com', 'superoof@databaseitf', 'Pin187932', 'itflab', 3306);
-$sql="DELETE FROM menu WHERE ID='$id'";
+$sql="DELETE FROM guestbook WHERE ID='$id'";
 $result=$con->query($sql);
 if($result){
   header('location:index.php');
