348d06dfd5fde3d05c6cdcb8cabc4ba219b35581
63070086 <69414436+63070086@users.noreply.github.com>
2020-11-14 00:03:31 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,55 +1,38 @@
 <?php
-include '../connect.php';
-include 'navbar.php';
-$sql="SELECT * FROM menu";
+include 'connect.php';
+$sql="SELECT * FROM guestbook";
 $result=$con->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
     <div class="container" style="width:1000px;margin-top:50px;">
-      <form class="form-inline" action="search.php" method="post">
-        <div class="form-group">
-        <label>รหัสอาหาร/ชื่ออาหาร</label>
-          <input type="text" name="keyword" class ="form-control mx-sm-2">
-          <input type="submit" name="search" value="ค้นหา" class="btn btn-outline-warning">
-        </div>
-      </form>
-
 <table class="table table-striped table-bordered">
   <tr class="bg-info">
-    <th>ลำดับที่</th>
-    <th>รหัสอาหาร</th>
-    <th>ชื่ออาหาร</th>
-    <th>ประเภทอาหาร</th>
-    <th>ราคา</th>
-    <th>การจัดการ</th>
+    <th>Link</th>
+    <th>Name</th>
+    <th>Commemt</th>
+    <th>Link</th>
+    <th>Edit and Delete</th>
   </tr>
     <?php
     $i=1;
     while ($row=mysqli_fetch_array($result)){
       ?>
       <tr>
-    <td><?php echo $i  ?></td>
-    <td><?php echo $row['menu_ID']?></td>
-    <td><?php echo $row['menu_Name']?></td>
-    <td><?php if($row['menu_Type']==1)
-  { echo "อาหารคาว";
-  }elseif ($row['menu_Type']==2)
-  {echo "อาหารหวาน";
-  }elseif (($row['menu_Type']==3))
-  {echo "อาหารว่าง";
-  }?></td>
-    <td><?php echo $row['menu_Price']?></td>
+    <td><?php echo $row['ID']?></td>
+    <td><?php echo $row['Name']?></td>
+    <td><?php echo $row['Comment']?></td>
+    <td><?php echo $row['Link']?></td>
     <td>
       <div class="btn-group" role="group" aria-label="Basic example">
-      <a href="edit_menu.php?menu_ID=<?php echo $row['menu_ID']?>" class="btn btn-secondary">แก้ไข</a>
-      <a href="del_menu.php?menu_ID=<?php echo $row['menu_ID']?>" class="btn btn-secondary"onclick="return confirm('ยืนยันการลบข้อมูล?')">ลบ</a>
+      <a href="edit_menu.php?ID=<?php echo $row['ID']?>" class="btn btn-secondary">แก้ไข</a>
+      <a href="del_menu.php?ID=<?php echo $row['ID']?>" class="btn btn-secondary"onclick="return confirm('ยืนยันการลบข้อมูล?')">ลบ</a>
       </div>
     </td>
   </tr>
