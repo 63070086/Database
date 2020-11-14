392a8ba1eeddad5667bdf43d4bb4cadc2a764bf1
63070086 <69414436+63070086@users.noreply.github.com>
2020-11-14 15:37:29 +0700

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,25 @@
+<?php
+$id=$_GET['ID'];
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'databaseitf.mysql.database.azure.com', 'superoof@databaseitf', 'Pin187932', 'itflab', 3306);
+$sql="SELECT * FROM guestbook WHERE ID='$id'";
+$row=mysqli_fetch_array($result);
+ ?>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<head>
+	<title>Edit Form</title>
+</head>
+<body>
+  <form action = "update.php?ID=<?php echo $row["ID"]; ?>" method = "post" id="CommentForm" class="form-group">
+    Name:<br>
+    <input type="text" name = "Name" id="idName" placeholder="Enter Name" value="<?php echo "$row[Name]"; ?>"> <br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name = "Comment" id="idComment" placeholder="Enter Comment" value="<?php echo "$row[Comment]"; ?>"></textarea><br>
+    Link:<br>
+    <input type="text" name = "Link" id="idLink" placeholder="Enter Link" value="<?php echo "$row[Link]"; ?>"> <br><br>
+    <input type="submit" id="commentBtn"class="btn btn-outline-warning">
+  </form>
+</body>
+</html>
