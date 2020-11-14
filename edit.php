0d2ac18c0965ebc726b799f0d76404e790c7549b
63070086 <69414436+63070086@users.noreply.github.com>
2020-11-14 17:09:35 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,17 +1,16 @@
 <?php
-include 'navbar.php';
-$id=$_GET['menu_ID'];
-include '../connect.php';
-$sql="SELECT * FROM menu WHERE menu_ID='$id'";
-$result=$con->query($sql);
-$row=mysqli_fetch_array($result);
+$id=$_GET['ID'];
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'databaseitf.mysql.database.azure.com', 'superoof@databaseitf', 'Pin187932', 'itflab', 3306);
+$row = mysqli_query($conn, "SELECT * FROM guestbook WHERE ID='$id'");
 ?>
-<form action = "update.php" method = "post" id="CommentForm" class="form-group">
+<form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm" class="form-group">
+
     Name:<br>
-    <input type="text" name = "name" id="idName" <br>
+    <input type="text" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" ></textarea><br>  
+    <textarea rows="10" cols="20" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>"></textarea><br>  
     Link:<br>
-    <input type="text" name = "link" id="idLink" > <br><br>
+    <input type="text" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
     <input type="submit" id="commentBtn"class="btn btn-outline-warning">
   </form> 
