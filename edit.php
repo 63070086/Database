484d6b3898efa91deec722f95111a5bdc81000b5
63070086 <69414436+63070086@users.noreply.github.com>
2020-11-14 18:00:38 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -8,10 +8,10 @@ $row = mysqli_fetch_array($res)
 <div class="container" style="width:600px;margin-top:50px;">
 <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm" class="form-group">
     Name:<br>
-    <input type="text" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
+    <input type="text" name = "Name" id="idName" value="<?php echo "$row['Name']"; ?>" <br>
     Comment:<br>
-    <input type="text" name = "Name" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
+    <input type="text" name = "Name" id="idComment" value="<?php echo "$row['Comment']"; ?>" <br>
     Link:<br>
-    <input type="text" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
+    <input type="text" name = "Link" id="idLink" value="<?php echo "$row['Link']"; ?>"> <br><br>
     <input type="submit" id="commentBtn"class="btn btn-outline-warning">
   </form>
