aab156b6d82b540d4db84b13142d66eba123b844
63070086 <69414436+63070086@users.noreply.github.com>
2020-11-14 15:57:11 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -3,16 +3,16 @@ $id=$_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'databaseitf.mysql.database.azure.com', 'superoof@databaseitf', 'Pin187932', 'itflab', 3306);
 $sql="SELECT * FROM guestbook WHERE ID='$id'";
-$row=mysqli_fetch_array($conn);
+$result=$con->query($sql);
+$row=mysqli_fetch_array($result);
  ?>
-<!DOCTYPE html>
+<?php echo "$row[ID]"; ?>
 <html>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <head>
 	<title>Edit Form</title>
 </head>
 <body>
-	<?php echo "$row[ID]"; ?>
   <form action = "update.php?ID=<?php echo $row["ID"]; ?>" method = "post" id="CommentForm" class="form-group">
     Name:<br>
     <input type="text" name = "Name" placeholder="Enter Name" value="<?php echo "$row[Name]"; ?>"> <br>
