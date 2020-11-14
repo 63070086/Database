c50dd8d6f6c333d10e11acbfaecd0aa7b38e8c1c
63070086 <69414436+63070086@users.noreply.github.com>
2020-11-14 16:03:30 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,15 +4,10 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'databaseitf.mysql.database.azure.com', 'superoof@databaseitf', 'Pin187932', 'itflab', 3306);
 $sql="SELECT * FROM guestbook WHERE ID='$id'";
 $row=mysqli_fetch_array($conn);
+echo "$row[ID]";
  ?>
-<!DOCTYPE html>
-<html>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<head>
-	<title>Edit Form</title>
-</head>
-<body>
-	<?php echo "$row[ID]"; ?>
+<div class="container" style="width:600px;margin-top:50px;">
   <form action = "update.php?ID=<?php echo $row["ID"]; ?>" method = "post" id="CommentForm" class="form-group">
     Name:<br>
     <input type="text" name = "Name" placeholder="Enter Name" value="<?php echo "$row[Name]"; ?>"> <br>
@@ -22,5 +17,5 @@ $row=mysqli_fetch_array($conn);
     <input type="text" name = "Link" placeholder="Enter Link" value="<?php echo "$row[Link]"; ?>"> <br><br>
     <input type="submit" id="commentBtn"class="btn btn-outline-warning">
   </form>
-</body>
+</div>
 </html>
